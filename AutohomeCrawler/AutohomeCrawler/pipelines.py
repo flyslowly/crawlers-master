1617eea6e0dcc336f8187161ead0f61c21b0c476
xiangxu <xiang.xu@volvocars.com>
2020-12-30 16:49:22 +0800

    Improved pipelines to operate forum item

--- AutohomeCrawler/AutohomeCrawler/pipelines.py
+++ AutohomeCrawler/AutohomeCrawler/pipelines.py
@@ -6,23 +6,40 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
 from pymongo import MongoClient
+from AutohomeCrawler.items import ForumItem
+from AutohomeCrawler.settings import MONGO_URI
 
 class AutohomecrawlerPipeline:
-    def __init__(self):
-        self.connection = MongoClient('localhost', 27017)
-        self.db = self.connection.autohome
-        self.forum = self.db.forum
-        self.qa = self.db.qa
-        self.article = self.db.article
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.client = None
+        self.db= None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=MONGO_URI,
+            mongo_db='autohome',
+        )
+    
+    def open_spider(self, spider):
+        self.client = MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
 
     def process_item(self, item, spider):
-        if not self.connection or not item:
-            return
-        self.forum.save(item)
+        if isinstance(item, ForumItem):
+            self._process_forum(item)
+        # elif isinstance(item, ZhihuRelationItem):
+        #     self._process_relation(item)
         return item
 
-    def __del__(self):
-        if self.connection:
-            self.connection.close()
+    def _process_forum(self, item):
+        collection = self.db['forum']
+        # collection.save(dict(item))
+        collection.save(item)
